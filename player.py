f14c878865a90dd8d7acfc198c944d12dec755a0
Filipa Andrade <fandrade@laterpay.net>
2024-04-03 11:55:02 +0200

    First try

--- player.py
+++ player.py
@@ -2,9 +2,20 @@
 class Player:
     VERSION = "Chubby Alpha Unicorn"
 
+    def check_cards(self):
+        ...
+
+    def bet(self):
+        ...
+
     def betRequest(self, game_state):
         if game_state["bet_index"] > 2 and game_state["pot"] < 2:
-            return 2
+            return game_state["current_buy_in"] + 1
+
+        #check_cards()
+
+        #bet()
+
         return 1
 
     def showdown(self, game_state):
