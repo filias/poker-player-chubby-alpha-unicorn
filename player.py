b18f14d4f4f97bf380c98786c8689efd225094f8
Filipa Andrade <fandrade@laterpay.net>
2024-04-03 12:20:13 +0200

    Add raise and call

--- player.py
+++ player.py
@@ -2,10 +2,7 @@
 class Player:
     VERSION = "Chubby Alpha Unicorn"
 
-    def check_cards(self):
-        ...
-
-    def bet(self):
+    def check_cards(self) -> bool:
         ...
 
     def get_other_active_players(self):
@@ -18,20 +15,30 @@ class Player:
             and player["id"] != self.game_state["in_action"]
         ]
 
+    @property
+    def call_bet(self):
+        return self.game_state["current_buy_in"] - self.game_state["players"]["in_action"]["bet"]
+
+    @property
+    def raise_bet(self):
+        return self.call_bet + 1
+
     def betRequest(self, game_state):
         # self.game_state should be immutable - don't change it
         self.game_state = game_state
 
+        # If there are other playing in the game we call
         other_players = self.get_other_active_players()
+        if len(other_players) > 0:
+            return self.call_bet
 
-        if len(other_players) > 0 and game_state["pot"] < 2:
-            return game_state["current_buy_in"] + 1
+        # If the cards are good we raise
+        if check_cards():
+            return self.raise_bet
 
-        # check_cards()
+        # Otherwise we call
+        return self.call_bet
 
-        #bet()
-
-        return game_state["current_buy_in"] + 1
 
     def showdown(self, game_state):
         pass
