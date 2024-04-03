5098a06a18666e72ccfa41ed6fc2dac3af2f4d6a
Filipa Andrade <fandrade@laterpay.net>
2024-04-03 11:44:58 +0200

    First try

--- player.py
+++ player.py
@@ -1,8 +1,10 @@
 
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = "Chubby Alpha Unicorn"
 
     def betRequest(self, game_state):
+        if game_state["bet_index"] > 2 and game_state["pot"] < 2:
+            return 1
         return 0
 
     def showdown(self, game_state):
